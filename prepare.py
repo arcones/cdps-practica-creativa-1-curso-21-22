6e68d75cbd3a2938f279e2813e78fd8b79965877
marta.rodrigueza <marta.rodrigueza@l078.lab.dit.upm.es>
2021-12-06 20:43:11 +0100

    WIP Load balancer

--- prepare.py
+++ prepare.py
@@ -13,8 +13,10 @@ init_logs()
 def prepare(CONFIG_FILE, num_serv):
     cleanup(CONFIG_FILE)
     _save_config_file(CONFIG_FILE, num_serv)
-    _create_qcows(num_serv)
-    _create_templates(num_serv)
+    _create_mv_qcows(num_serv)
+    _create_lb_qcow()
+    _create_mv_xml(num_serv) # TODO Create also the client C1
+    _create_lb_xml()
 
 
 def _clean_up_from_previous_runs():
@@ -40,7 +42,8 @@ def _save_config_file(CONFIG_FILE, num_serv):
     log_info("El fichero json ha sido almacenado")
 
 
-def _create_qcows(num_serv):
+def _create_mv_qcows(num_serv):
+    log_info("Creando los ficheros qcow2 requeridos para los servidores...")
     i = 1
     while i <= num_serv:
         log_info(f"Creando el fichero qcow2 de la máquina {i}")
@@ -48,13 +51,14 @@ def _create_qcows(num_serv):
                         "-b", "cdps-vm-base-pc1.qcow2", f"s{i}.qcow2"], stdout=subprocess.DEVNULL)
         i += 1
 
-    log_info("Los ficheros qcow2 requeridos han sido creados")
+    log_info("Los ficheros qcow2 requeridos para los servidores han sido creados")
 
 
-def _create_templates(num_serv):
+def _create_mv_xml(num_serv):
+    log_info("Creando los ficheros xml de configuración requeridos para los servidores...")
     i = 1
     while i <= num_serv:
-        log_info(f"Creando la plantilla de configuración de la máquina {i}")
+        log_info(f"Creando el fichero xml de configuración de la máquina {i}")
         subprocess.call(["cp", "plantilla-vm-pc1.xml", f"s{i}.xml"])
 
         with open(f"s{i}.xml", "r") as xml:
@@ -69,4 +73,26 @@ def _create_templates(num_serv):
 
         i += 1
 
-    log_info("Las plantillas de configuración requeridas han sido creadas")
+    log_info("Los ficheros xml de configuración requeridos para los servidores han sido creados")
+
+def _create_lb_qcow():
+    log_info("Creando el fichero qcow2 requerido para el balanceador de carga...")
+    subprocess.call(["qemu-img", "create", "-f", "qcow2", "-b", "cdps-vm-base-pc1.qcow2", f"lb.qcow2"], stdout=subprocess.DEVNULL)
+    log_info("El fichero qcow2 requerido para el balanceador de carga ha sido creado")
+
+def _create_lb_xml():
+    log_info(f"Creando el fichero de configuración del balanceador de carga...")
+    subprocess.call(["cp", "plantilla-vm-pc1.xml", f"lb.xml"])
+
+    with open(f"lb.xml", "r") as xml:
+        xml_content = xml.read()
+
+    xml_content = xml_content.replace('<name>XXX</name>', f'<name>lb</name>')
+    xml_content = xml_content.replace('/mnt/tmp/XXX/XXX.qcow2',f'{os.getcwd()}/lb.qcow2')
+    xml_content = xml_content.replace("bridge='XXX'", f"bridge='LAN2'")
+    
+    with open(f"lb.xml", 'w') as xml:
+        xml.write(xml_content)
+
+
+    log_info("El fichero xml de configuración requerido han sido creado")
