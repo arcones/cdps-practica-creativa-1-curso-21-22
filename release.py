abd5504621eba42edb25a2eb2da0049af062d37a
marta.rodrigueza <marta.rodrigueza@l078.lab.dit.upm.es>
2021-12-09 20:36:58 +0100

    Polish release command

--- release.py
+++ release.py
@@ -22,12 +22,14 @@ def _destroy_vms(num_serv):
     i = 1
     while i <= num_serv:
         subprocess.call(["sudo", "virsh", "destroy", f"s{i}"])
+        subprocess.call(["sudo", "virsh", "undefine", f"s{i}"])
         i += 1
     log_info(f"Destruidos los {num_serv} servidores en forma de máquinas virtuales")
 
 def _destroy_lb():
     log_info("Destruyendo el balanceador de carga en forma de máquina virtual...")
-    subprocess.call(["sudo", "virsh", "destroy", "lb"], stderr=subprocess.DEVNULL)
+    subprocess.call(["sudo", "virsh", "destroy", "lb"])
+    subprocess.call(["sudo", "virsh", "undefine", "lb"])
     log_info("Destruido el balanceador de carga en forma de máquina virtual")
 
 
@@ -35,18 +37,18 @@ def _cleanup_qcows():
     log_info("Borrando ficheros qcows de ejecuciones anteriores (si existiesen)...")
     i = 1
     while i <= 5:
-        subprocess.call(["rm", f"s{i}.qcow2"], stderr=subprocess.DEVNULL)
+        subprocess.call(["rm", "-rf", f"s{i}.qcow2"], stderr=subprocess.DEVNULL)
         i += 1
-    subprocess.call(["rm", f"lb.qcow2"], stderr=subprocess.DEVNULL)
+    subprocess.call(["rm", "-rf", f"lb.qcow2"], stderr=subprocess.DEVNULL)
     log_info("Borrados los ficheros qcows de ejecuciones anteriores")
 
-def _cleanup_xmls():
+def _cleanup_xmls():#TODO split in methods, lb and servers? same for above method
     log_info("Borrando ficheros xmls de ejecuciones anteriores (si existiesen)...")
     i = 1
     while i <= 5:
-        subprocess.call(["rm", f"s{i}.xml"], stderr=subprocess.DEVNULL)
+        subprocess.call(["rm", "-rf", f"s{i}.xml"], stderr=subprocess.DEVNULL)
         i += 1
-    subprocess.call(["rm", f"lb.xml"], stderr=subprocess.DEVNULL)
+    subprocess.call(["rm", "-rf", f"lb.xml"], stderr=subprocess.DEVNULL)
     log_info("Borrados los ficheros xmls de ejecuciones anteriores")
 
 def _cleanup_config(CONFIG_FILE):
