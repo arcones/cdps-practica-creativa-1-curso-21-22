0366e0f8f4d11907f110eba7d7941f14746982e5
marta.rodrigueza <marta.rodrigueza@l078.lab.dit.upm.es>
2021-12-12 18:38:24 +0100

    Haproxy working

--- configure.py
+++ configure.py
@@ -22,6 +22,7 @@ def configure(num_serv):
     _update_lb_network_interface()
     _update_host_configuration()
     _update_indexes(num_serv)
+    _update_lb_haproxy(num_serv)
 
     LOGGER.info("El escenario ha sido configurado")
     
@@ -73,6 +74,55 @@ def _update_lb_network_interface():
     with open(f"{TMP_DIR}/sysctl.conf", 'w') as sysctl:
         sysctl.write("net.ipv4.ip_forward=1\n")
     subprocess.call(["sudo", "virt-copy-in", "-a", f"lb.qcow2", f"{TMP_DIR}/sysctl.conf", "/etc"])
+
+    subprocess.call(["rm", "-rf", TMP_DIR])
+
+def _update_lb_haproxy(num_serv):
+    subprocess.call(["mkdir", "-p", TMP_DIR])
+    with open(f"{TMP_DIR}/haproxy.cfg", 'a') as haproxy:
+        haproxy.write("global\n")
+        haproxy.write("log /dev/log	local0\n")
+        haproxy.write("log /dev/log	local1 notice\n")
+        haproxy.write("chroot /var/lib/haproxy\n")
+        haproxy.write("stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners\n")
+        haproxy.write("stats timeout 30s\n")
+        haproxy.write("user haproxy\n")
+        haproxy.write("group haproxy\n")
+        haproxy.write("daemon\n")
+        haproxy.write("ca-base /etc/ssl/certs\n")
+        haproxy.write("crt-base /etc/ssl/private\n")
+        haproxy.write("ssl-default-bind-ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384\n")
+        haproxy.write("ssl-default-bind-ciphersuites TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256\n")
+        haproxy.write("ssl-default-bind-options ssl-min-ver TLSv1.2 no-tls-tickets\n")
+        haproxy.write("defaults\n")
+        haproxy.write("log	global\n")
+        haproxy.write("mode	http\n")
+        haproxy.write("option	httplog\n")
+        haproxy.write("option	dontlognull\n")
+        haproxy.write("timeout connect 5000\n")
+        haproxy.write("timeout client  50000\n")
+        haproxy.write("timeout server  50000\n")
+        haproxy.write("errorfile 400 /etc/haproxy/errors/400.http\n")
+        haproxy.write("errorfile 403 /etc/haproxy/errors/403.http\n")
+        haproxy.write("errorfile 408 /etc/haproxy/errors/408.http\n")
+        haproxy.write("errorfile 500 /etc/haproxy/errors/500.http\n")
+        haproxy.write("errorfile 502 /etc/haproxy/errors/502.http\n")
+        haproxy.write("errorfile 503 /etc/haproxy/errors/503.http\n")
+        haproxy.write("errorfile 504 /etc/haproxy/errors/504.http\n")
+        haproxy.write("frontend lb\n")
+        haproxy.write("\tbind *:80\n")
+        haproxy.write("\tmode http\n")
+        haproxy.write("\tdefault_backend webservers\n")
+        haproxy.write("backend webservers\n")
+        haproxy.write("\tmode http\n")
+        haproxy.write("\tbalance roundrobin\n")
+        i = 1
+        while i <= num_serv:
+            haproxy.write(f"\tserver s{i} 10.10.2.1{i}:80 check\n")
+            i += 1
+
+    subprocess.call(["sudo", "virt-copy-in", "-a", f"lb.qcow2", f"{TMP_DIR}/haproxy.cfg", "/etc/haproxy/"])
+    subprocess.call(["sudo", "virt-cat", "-a", f"lb.qcow2", "/etc/haproxy/haproxy.cfg"])
 
     subprocess.call(["rm", "-rf", TMP_DIR])
 
