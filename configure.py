afd973d8498a0df30ea6cdc9690e5ba0408019b3
marta.rodrigueza <marta.rodrigueza@l078.lab.dit.upm.es>
2021-12-12 17:50:32 +0100

    Solve connectivity problems

--- configure.py
+++ configure.py
@@ -20,6 +20,7 @@ def configure(num_serv):
     _update_hosts(servers_and_lb)
     _update_server_network_interfaces(num_serv)
     _update_lb_network_interface()
+    _update_host_configuration()
 
     LOGGER.info("El escenario ha sido configurado")
     
@@ -51,6 +52,7 @@ def _update_server_network_interfaces(num_serv):
             interfaces.write("iface eth0 inet static\n")
             interfaces.write(f"\taddress 10.10.2.1{i}\n")
             interfaces.write("\tnetmask 255.255.255.0\n")
+            interfaces.write("\tgateway 10.10.2.1\n")
             interfaces.write("\tup route add default via 10.10.2.1 dev eth0\n")
         subprocess.call(["sudo", "virt-copy-in", "-a", f"s{i}.qcow2", f"{TMP_DIR}/interfaces", "/etc/network/"])
         subprocess.call(["sudo", "virt-cat", "-a", f"s{i}.qcow2", "/etc/network/interfaces"]) # TODO remove
@@ -65,7 +67,7 @@ def _update_lb_network_interface():
         interfaces.write("\taddress 10.10.1.1\n")
         interfaces.write("\tnetmask 255.255.255.0\n\n")
         interfaces.write("auto eth1\n")
-        interfaces.write("iface eth0 inet static\n")
+        interfaces.write("iface eth1 inet static\n")
         interfaces.write("\taddress 10.10.2.1\n")
         interfaces.write("\tnetmask 255.255.255.0\n")
     subprocess.call(["sudo", "virt-copy-in", "-a", f"lb.qcow2", f"{TMP_DIR}/interfaces", "/etc/network/"])
@@ -77,3 +79,18 @@ def _update_lb_network_interface():
     subprocess.call(["sudo", "virt-cat", "-a", f"lb.qcow2", "/etc/sysctl.conf"]) # TODO remove
 
     subprocess.call(["rm", "-rf", TMP_DIR])
+
+def _update_host_configuration():
+    subprocess.call(["sudo", "ifconfig", "LAN1", "10.10.1.3/24"])
+    subprocess.call(["sudo", "ip", "route", "add", "10.10.0.0/16", "via", "10.10.1.1"])
+    # Esto para c1
+    #subprocess.call(["mkdir", "-p", TMP_DIR])
+    #with open(f"{TMP_DIR}/interfaces", 'w') as interfaces:
+    #    interfaces.write("auto LAN1\n")
+    #    interfaces.write("iface LAN1 inet static\n")
+    #    interfaces.write("\taddress 10.10.1.3\n")
+    #    interfaces.write("\tnetmask 255.255.255.0\n")
+    #    interfaces.write("\tgateway 10.10.1.1\n")
+    #    interfaces.write("\tup route add 10.10.0.0/16 via 10.10.1.1 dev LAN1\n")
+    #subprocess.call(["sudo", "mv", f"{TMP_DIR}/interfaces", "/etc/network/"])
+    #subprocess.call(["rm", "-rf", TMP_DIR])
